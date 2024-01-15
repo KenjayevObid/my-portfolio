f2da9a90c639f96bef139602da62f64fb225c3d8
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2024-01-15 17:01:13 +0500

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <div className="flex">
+        <ul className="list-disc pl-2">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+        <ul className="list-disc pl-8">
+          <li>React JS</li>
+          <li>Next JS</li>
+          <li>Node.js</li>
+        </ul>
+        <ul className="list-disc pl-8">
+          <li>Material UI</li>
+          <li>Tailwind CSS</li>
+          <li>MongoDB</li>
+        </ul>
+        <ul className="list-disc pl-8">
+          <li>Express</li>
+          <li>Netlify</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>University of California, Santa Cruz</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/web.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a web developer with a passion for creating interactive and
+            responsive web applications. I have experience working with
+            JavaScript, React, Next js, Node.js, Express, MongoDB,
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
