bf92af7479d9ad9bccb4f5e2c011adafe2872c46
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2024-01-16 15:54:40 +0500

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-8  place-self-center text-center sm:text-left  justify-self-start"
         >
           <h1 className="text-white mb-4 text-3xl sm:text-3xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
@@ -28,15 +28,15 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+            esse tenetur in recusandae, quisquam ipsam.
           </p>
           <div>
-            
             <Link
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] sm:w-full hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
             </Link>
